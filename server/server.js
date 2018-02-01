5d8378a0dc65321c9027fc89c17e4d7dc6aace61
Brandon <brandontkessler@gmail.com>
2018-01-31 22:37:33 -0800

    Initial git push Live

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const http = require('http');
+
+const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+
+
+
+
+
+
+
+
+
+
+server.listen(port, () => {
+  console.log(`Started port ${port}.`);
+});
