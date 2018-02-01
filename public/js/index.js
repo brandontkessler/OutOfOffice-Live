7d9e4bd9be6d74c3a65663a5f6e7e459ed967581
Brandon <brandontkessler@gmail.com>
2018-02-01 00:58:45 -0800

    Added scroll function to chat

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,23 @@
 var socket = io();
 
+function scrollToBottom() {
+  //Selectors
+  var messages = $('#messages');
+  var newMessage = messages.children('li:last-child');
+  //Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+};
+
 socket.on('connect', function () {
   console.log('Connected to server');
-
 });
 
 socket.on('disconnect', function () {
@@ -17,6 +32,7 @@ socket.on('newMessage', function(message) {
   });
 
   $('#messages').append(html);
+  scrollToBottom();
 
   // console.log('New message', message)
   // var li = $('<li></li>');
