3e8cb5f6bf7747fbd1a5c82cf4699198e8e2049e
Brandon <brandontkessler@gmail.com>
2018-02-01 00:32:49 -0800

    major updates

---
+++ public/js/index.js
@@ -0,0 +1,31 @@
+var socket = io();
+
+socket.on('connect', function () {
+  console.log('Connected to server');
+
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server')
+});
+
+socket.on('newMessage', function(message) {
+  console.log('New message', message)
+  var li = $('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  $('#messages').append(li);
+});
+
+
+var messageTextbox = $('[name=message]');
+$('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'user',
+    text: messageTextbox.val()
+  }, function() {
+    messageTextbox.val('');
+  });
+});
--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 const app = express();
@@ -12,15 +14,19 @@ const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+  console.log('New User')
 
+  socket.on('createMessage', (message, callback) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback();
+  });
 
-
-
-
-
-
-
-
+  socket.on('disconnect', (socket) => {
+    console.log('User left')
+  })
+})
 
 server.listen(port, () => {
   console.log(`Started port ${port}.`);
